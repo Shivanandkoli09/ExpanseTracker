93753decf16de8e053af746e513ad1adf535f05d
shivanand <shivanand.koli@kpit.com>
2025-07-30 23:37:39 +0530

    Implemented the Authentication Using firebase.

---
+++ ExpanseTracker/View/AuthenticationView/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ExpanseTracker
+//
+//  Created by KPIT on 29/07/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ ExpanseTracker/ViewModel/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  ExpanseTracker
+//
+//  Created by KPIT on 29/07/25.
+//
+
+import Foundation
